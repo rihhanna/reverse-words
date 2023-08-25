02a35b2e9ba35e7c36839204185e48faa82a2161
rihhana <hrihhana@gmail.com>
2023-08-25 18:12:08 +0300

    done it

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Good Luck 💪🏾
+function reverseWords(sentence) {
+    // split the sentence by spaces
+    let words = sentence.split(" ");
+    // reverse the order of the words
+    words.reverse();
+    // join the words by spaces
+    let reversed = words.join(" ");
+    // return the reversed sentence
+    return reversed;
+  }
+
+ console.log(reverseWords("fikrcamp bootcamp"))  
+ console.log(reverseWords("hello there!"))
+ console.log(reverseWords("welcome to my city"))
